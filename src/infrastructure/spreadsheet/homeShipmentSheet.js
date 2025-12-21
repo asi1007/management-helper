075bf0f679d563d6213b10a6683607b16f90c156
wada <zyanzyakazyan@gmail.com>
2025-12-21 16:39:18 +0900

    refactor: make getColumnIndex private method

--- src/infrastructure/spreadsheet/homeShipmentSheet.js
+++ src/infrastructure/spreadsheet/homeShipmentSheet.js
@@ -6,7 +6,7 @@ class HomeShipmentSheet extends BaseSheet {
     this.headers = this.data[1]; 
   }
 
-  getColumnIndex(columnName) {
+  _getColumnIndex(columnName) {
     const index = this.headers.indexOf(columnName);
     if (index === -1) {
       throw new Error(`列 "${columnName}" が見つかりません`);
@@ -15,42 +15,22 @@ class HomeShipmentSheet extends BaseSheet {
   }
 
   getValue(rowID, columnName) {
-    const columnIndex = this.getColumnIndex(columnName);
+    const columnIndex = this._getColumnIndex(columnName);
     return this.sheet.getRange(rowID, columnIndex + 1).getValue();
   }
 
-  getActiveValues(columnName) {
-    const selectedRowNumbers = this._getSelectedRowNumbers(this.sheet);
-    const sortedRowNumbers = Array.from(selectedRowNumbers).sort((a, b) => a - b);
-    const columnIndex = this.getColumnIndex(columnName);
-    const values = [];
-
-    for (const rowNum of sortedRowNumbers) {
-      const isHidden = this.sheet.isRowHiddenByFilter(rowNum) || this.sheet.isRowHiddenByUser(rowNum);
-      if (isHidden) {
-        continue;
-      }
-      const value = this.sheet.getRange(rowNum, columnIndex + 1).getValue();
-      values.push(value);
-    }
-
-    return values;
+  getValues(columnName) {
+    const columnIndex = this._getColumnIndex(columnName);
+    return this.data.map(row => row[columnIndex]);
   }
 
   getRowIdsByTracking() {
-    const trackingValues = this.getActiveValues('追跡番号');
-    if (trackingValues.length === 0) {
-      return [];
-    }
+    activeRows = this.getActiveRowData();
+    const trackingValues = getActiveValues('追跡番号');
     
-    const trackingColumnIndex = this.getColumnIndex('追跡番号');
-    const rowIdColumnIndex = this.getColumnIndex('行番号');
-    const rowIds = [];
-    
-    // アクティブな行から取得した追跡番号のいずれかに一致する行の行IDを取得
     for (let i = 0; i < this.data.length; i++) {
-      const rowTrackingNumber = String(this.data[i][trackingColumnIndex]);
-      if (trackingValues.some(tv => String(tv) === rowTrackingNumber)) {
+      rowIds = tracking ("行番号");
+      if (String(this.data[i][trackingColumnIndex]) === String(trackingNumber)) {
         rowIds.push(this.data[i][rowIdColumnIndex]);
       }
     }
